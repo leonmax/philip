d492c25417423aad2b1497f121c30a7bf51cd88b
Kevin Schoon <kevinschoon@gmail.com>
2015-09-15 18:03:20 -0400

    Refactored Config class

--- philip/config.py
+++ philip/config.py
@@ -17,12 +17,13 @@ class Config:
 
     PROVIDERS = ['marathon', 'ecs']
 
-    _marathon_server = namedtuple('Server', ['name', 'url', 'username', 'password'])
+    _marathon_config = namedtuple('Server', ['name', 'url', 'username', 'password'])
+    _ecs_config = namedtuple('ECS_Config', ['cluster', 'aws_options'])
 
     def __init__(self):
         self.config = {}
 
-    def _get_config(self, config_path=None):
+    def _load_config(self, config_path=None):
         if not config_path:
             for default_path in self.DEFAULT_CONFIG_FILES:
                 if path.exists(path.expanduser(default_path)):
@@ -47,42 +48,28 @@ class Config:
         return profile['provider']
 
     def _get_marathon_server(self, provider):
-        print(provider)
-        return self._marathon_server(provider, provider['url'], provider['username'], provider['password'])
+        return self._marathon_config(provider, provider['url'], provider['username'], provider['password'])
 
-    def _get_ecs_cluster(self, profile):
-        pass
+    def _get_ecs_config(self, provider):
+        return self._ecs_config(provider['cluster'], provider['aws_config'])
 
-    def get(self, profile_names, config_path=None):
+    def _get_config(self, profile, provider_name=None):
+        provider = self._get_provider(profile)
+        if provider_name:
+            if provider_name != provider['provider_type']:  # TODO
+                raise PhilipException('This profile does not support provider {}'.format(provider_name))
+        if provider_name == 'marathon':
+            return self._get_marathon_server(provider)
+        if provider_name == 'ecs':
+            return self._get_ecs_config(provider)
+
+    def get(self, profile_names, config_path=None, provider_name=None):
         if not self.config or config_path:
-            self._get_config(config_path)
+            self._load_config(config_path)
         if 'profiles' not in self.config:
             raise PhilipException('You must specify at least one "profile" in your configuration')
         for profile in reversed(profile_names):
             profile = self._get_profile(profile)
             if profile:
-                provider = self._get_provider(profile)
-                if provider['provider_type'] == 'marathon':
-                    return self._get_marathon_server(provider)
+                return self._get_config(profile, provider_name)
         raise PhilipException('Unable to load configuration')
-
-
-"""
-Server = namedtuple('Server', ['name', 'url', 'username', 'password'])
-
-
-def load_server(profile_names, conffile=None):
-    if not conffile:
-        for default_path in DEFAULT_CONFIG_FILES:
-            if path.exists(path.expanduser(default_path)):
-                conffile = path.expanduser(default_path)
-                break
-        else:
-            raise PhilipException('no config file exists under /etc/philip or ~/.config/philip')
-    with open(conffile, 'r') as fp:
-        config = yaml.load(fp.read())
-        for profile in reversed(profile_names):
-            if profile in config:
-                server_config = config[profile]
-                return Server(profile, server_config['url'], server_config['username'], server_config['password'])
-"""
